7e5ef6d9565d0d6ef9085d64fd5b0c24b184f5f3
Luther Isaboke <lusaboke@gmail.com>
2024-05-28 23:14:27 +0300

    py loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
+# The happy_new_year() function uses a while loop to count down from 10 to 1
+# and prints each number. After reaching 1, it prints "Happy New Year!"
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+        print(i)
+        i = i - 1
+    print("Happy New Year!")
 
+# The square_integers() function takes a list of integers as input
+# and returns a new list with the squared values of each integer
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for num in int_list:
+        squared_list.append(num ** 2)
+    return squared_list
 
+# The fizzbuzz() function prints the numbers from 1 to 100
+# If the number is divisible by 3, it prints "Fizz"
+# If the number is divisible by 5, it prints "Buzz"
+# If the number is divisible by both 3 and 5, it prints "FizzBuzz"
+# Otherwise, it just prints the number
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
